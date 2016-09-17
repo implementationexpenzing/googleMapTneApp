f97a7c79615e89cac661d1acfe342cbb1c74ec98
magneto.1990@gmail.com <magneto.1990@gmail.com>
2016-09-17 17:30:08 +0530

    sdf

--- js/index.js
+++ js/index.js
@@ -2054,16 +2054,34 @@ function validateValidMobileUser(){
 	   });
 	}
 }
+document.addEventListener("deviceready", function() {
+  // Define a div tag with id="map_canvas"
+  var mapDiv = document.getElementById("map_canvas");
 
+  // Initialize the map plugin
+  var map = plugin.google.maps.Map.getMap(mapDiv);
+
+  // You have to wait the MAP_READY event.
+  map.on(plugin.google.maps.event.MAP_READY, onMapInit);
+});
 
 function btnClick(){
 	alert("inside btnClick")
 	var map = plugin.google.maps.Map.getMap();
 	alert(map)
-	map.addEventListener(plugin.google.maps.event.MAP_READY, function onMapInit(map) {
-  	// The map is initialized, then show a map dialog
-  	map.showDialog();
-	});
+	document.addEventListener("deviceready", function() {
+  // Define a div tag with id="map_canvas"
+  var mapDiv = document.getElementById("map_canvas");
+
+  // Initialize the map plugin
+  var map = plugin.google.maps.Map.getMap(mapDiv);
+
+  // You have to wait the MAP_READY event.
+  map.on(plugin.google.maps.event.MAP_READY, onMapInit);
+});
+
+function onMapInit(map) {
+}
 }
 /*function attachGoogleSearchBox(component){
 	alert("attachGoogleSearchBox")
